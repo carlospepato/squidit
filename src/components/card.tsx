a1009635d7c237a1930e775d85768dd0a45849c5
carlospepato <71358695+carlospepato@users.noreply.github.com>
2024-08-05 11:16:15 -0300

    ajustando tamanho das imagens

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -11,15 +11,25 @@ interface CardProps {
 
 export function Card({ img, username, link, likes, comments, createAt }: CardProps) {
   return (
-    <div className='group relative w-full h-full sm:max-w-xs md:max-w-sm lg:max-w-md'>
+    <div className='group relative w-full h-full sm:max-w-xs md:max-w-sm lg:max-w-md overflow-hidden'>
       <a href={link}>
-        <img src={img} alt="" className='w-full h-auto' />
+        <img
+          src={img}
+          alt=""
+          className='w-full h-full object-cover'
+        />
         <div className='absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
           <div className="flex flex-col items-center gap-3">
             <span>@{username}</span>
-            <span className="text-sm w-full flex items-center justify-center gap-2"><Heart size={20} fill="white" />{likes}</span>
-            <span className="text-sm w-full flex items-center justify-center gap-2"><MessageCircle size={20} fill="white" />{comments}</span>
-            <span className="text-sm w-full flex items-center justify-center gap-2"><CalendarDays size={20} />{createAt}</span>
+            <span className="text-sm w-full flex items-center justify-center gap-2">
+              <Heart size={20} fill="white" />{likes}
+            </span>
+            <span className="text-sm w-full flex items-center justify-center gap-2">
+              <MessageCircle size={20} fill="white" />{comments}
+            </span>
+            <span className="text-sm w-full flex items-center justify-center gap-2">
+              <CalendarDays size={20} />{createAt}
+            </span>
           </div>
         </div>
       </a>
